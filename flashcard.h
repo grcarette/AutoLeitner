277083ed8e52124aac22f89ccf2d875422202893
grcarette <grcarette1@gmail.com>
2023-02-10 21:53:26 -0500

    Added flashcard class

--- flashcard.h
+++ flashcard.h
@@ -1,1 +1,43 @@
 #pragma once
+
+#include<string>
+
+using namespace std;
+
+class FlashCard {
+private:
+	string question;
+	string answer;
+	int conSuc;
+	int priority;
+public:
+	string getQuestion() {
+		return question;
+	}
+	string getAnswer() {
+		return answer;
+	}
+	int getConSuc() {
+		return conSuc;
+	}
+	int getPriority() {
+		return priority;
+	}
+
+	void setQuestion(string newQuestion) {
+		question = newQuestion;
+	}
+
+	void setAnswer(string newAnswer) {
+		answer = newAnswer;
+	}
+
+	void setConSuc(bool correct) {
+		if (correct == true) {
+			conSuc++;
+		}
+		else {
+			conSuc = 0;
+		}
+	}
+};
